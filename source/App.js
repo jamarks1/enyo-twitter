ffe70a2a549f50951f6be6d779ef2a8f39ee7edf
James Marks <jamarks1@gmail.com>
2012-10-20 17:33:53 -0400

    added a honey object. Just playing around

--- source/App.js
+++ source/App.js
@@ -2,9 +2,20 @@ enyo.kind({
 	name: "App",
 	fit: true,
 	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
+		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"},
+		{name: "honey", content: "Hi Honey!", style: 'color: blue'}
 	],
 	helloWorldTap: function(inSender, inEvent) {
 		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
 	}
 });
+
+enyo.kind({
+	name: "honey",
+	kind: enyo.Control,
+	tag: 'p',
+	content: 'Hi Honey!',
+	style: 'color: blue'
+});
+
+
