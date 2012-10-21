eed3fe92515135bce8f9872edf6e3f7591794abe
James Marks <jamarks1@gmail.com>
2012-10-21 15:01:11 -0400

    Spinning my wheels here

--- source/App.js
+++ source/App.js
@@ -1,21 +1,9 @@
-enyo.kind({
-	name: "App",
-	fit: true,
-	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"},
-		{name: "honey", content: "Hi Honey!", style: 'color: blue'}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
-	}
-});
 
 enyo.kind({
-	name: "honey",
-	kind: enyo.Control,
-	tag: 'p',
-	content: 'Hi Honey!',
-	style: 'color: blue'
+	name: "recipe",
+	kind: "FittableRows",
+	style: "height: 40%",
+	content: "Another box below"
 });
 
 
