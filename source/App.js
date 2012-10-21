90253645f38f45723665b2989e2ed23db362fdf8
James Marks <jamarks1@gmail.com>
2012-10-21 16:25:08 -0400

    Doing the tutorial for real now, okay?

--- source/App.js
+++ source/App.js
@@ -1,9 +1,15 @@
 
-enyo.kind({
-	name: "recipe",
-	kind: "FittableRows",
-	style: "height: 40%",
-	content: "Another box below"
+var list = new enyo.Control({
+	tag: "div",
+	components: [
+		{ tag: "p", content: "The Additive Primary Colors" },
+		{ tag: "ul", components: [
+			{tag: "li", name: "red", content: "red", style: "color: red"},
+			{tag: "li", name: "green", content: "green"},
+			{tag: "li", name: "blue", content: "blue"}]}]
 });
 
+list.$.green.applyStyle("color", "green");
+list.$.blue.applyStyle("color", "blue");
+	
 
